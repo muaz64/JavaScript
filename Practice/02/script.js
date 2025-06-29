13c40ced9d554f8918ae85dd6e68e30500fb3c45
muaz64 <muazmuhammad21@gmail.com>
2025-06-29 23:35:11 +0600

    practice d02 2.4

--- Practice/02/script.js
+++ Practice/02/script.js
@@ -9,3 +9,8 @@ const arr2 = [...arr1, 4, 5]; // [1, 2, 3, 4, 5]
 //Copy or Merge Arrays:
 const copy = [...arr1];
 const merged = [...arr1, ...arr2];
+
+//Spread in Objects:
+const obj1 = { a: 1 };
+const obj2 = { ...obj1, b: 2 }; // { a: 1, b: 2 }
+
