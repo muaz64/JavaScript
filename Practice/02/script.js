57cdc7446f88ea79d9f8ddee8e956b2483e2472a
muaz64 <muazmuhammad21@gmail.com>
2025-06-29 23:28:31 +0600

    practice d02 2.2

--- Practice/02/script.js
+++ Practice/02/script.js
@@ -1,2 +1,7 @@
+// Array
 const fruits = ["Apple", "Banana", "Mango"];
 fruits.forEach((fruit) => console.log(fruit));
+
+// Spread Operator
+const arr1 = [1, 2, 3];
+const arr2 = [...arr1, 4, 5]; // [1, 2, 3, 4, 5]
