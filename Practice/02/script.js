fd75837641569f5dc01b55274791291c273dca58
muaz64 <muazmuhammad21@gmail.com>
2025-06-29 23:33:11 +0600

    practice d02 2.3

--- Practice/02/script.js
+++ Practice/02/script.js
@@ -5,3 +5,7 @@ fruits.forEach((fruit) => console.log(fruit));
 // Spread Operator
 const arr1 = [1, 2, 3];
 const arr2 = [...arr1, 4, 5]; // [1, 2, 3, 4, 5]
+
+//Copy or Merge Arrays:
+const copy = [...arr1];
+const merged = [...arr1, ...arr2];
