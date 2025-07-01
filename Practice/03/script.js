5ca534764ae70e87a69d7fb4f733653357d41868
muaz64 <muazmuhammad21@gmail.com>
2025-07-01 22:44:55 +0600

    practice 3.3

---
+++ Practice/03/script.js
@@ -0,0 +1,22 @@
+// Map in ES6
+const muaz = new Map();
+muaz.set('fullname', 'Muaz Muhammad')
+// muaz.get('fullname');
+// muaz.has('fullname');
+// muaz.clear();
+const person = new Map();
+person.set('name', 'Muaz Muhammad');
+person.set('job', 'Teacher');
+person.set('color', 'blue');
+person.set('language', 'javascript');
+person.set('device', 'Dell');
+
+console.log(person);
+console.log(person.size);
+
+person.forEach((value, key) => console.log(`key is: ${key} and the value is : ${value}`));
+
+for(let[key, value] of person.entries()) {
+    console.log(`key is: ${key} and the value is : ${value}`);
+}
+
